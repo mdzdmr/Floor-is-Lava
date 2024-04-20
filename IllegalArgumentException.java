e649195caa00281a980cf1687261b5667fdc12f4
Mohammed Zaid Mir <mzaidmir111@gmail.com>
2024-04-20 14:55:59 -0400

    create-IllegalArgumentException.java

---
+++ IllegalArgumentException.java
@@ -0,0 +1,11 @@
+public class IllegalArgumentException extends RuntimeException
+{
+  /**
+   * Sets up this exception with an appropriate message.
+   * @param message explains the error that threw the exception
+   */
+  public IllegalArgumentException (String message)
+  {
+    super (message);
+  }
+}
